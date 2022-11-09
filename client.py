13e7cea3c039d1c835977f1daf72bab163e2e6db
Raph-Romeo <95073137+Raph-Romeo@users.noreply.github.com>
2022-11-09 12:05:12 +0100

    Update client.py

--- client.py
+++ client.py
@@ -22,8 +22,9 @@ def connect():
         return client_socket
 
 
-def disconnect():
+def disconnect(client_socket):
     global stop_threads
+    client_socket.close()
     stop_threads = True
 
 def recieve(client_socket):
@@ -32,6 +33,41 @@ def recieve(client_socket):
         print(data)
         if stop_threads:
             break
+
+
+def write(client_socket, username):
+    while True:
+        try:
+            msg = input(f"{username}> ")
+            if msg == '/disconnect':
+                disconnect(client_socket)
+            else:
+                message =f'{username}> {msg}'
+                client_socket.send(message.encode())
+        except:
+            disconnect(client_socket)
+            sys.exit(-2)
+        if stop_threads:
+            break
+
+
+def main():
+    username = input('Insert username: ')
+    client_socket = connect()
+    T1 = threading.Thread(target=write, args=[client_socket, username])
+    T2 = threading.Thread(target=recieve, args=[client_socket])
+    T1.start()
+    T2.start()
+    T1.join()
+    T2.join()
+    print('Successfully disconnected from socket')
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    threads = []
+    stop_threads = False
+    main()
 
 
 def write(client_socket, username):
