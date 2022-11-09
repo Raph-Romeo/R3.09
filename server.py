2c4111e32494b02ba247c376b2024a0da6bb9a52
Raph-Romeo <95073137+Raph-Romeo@users.noreply.github.com>
2022-11-09 12:00:34 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,35 @@
+import threading
+from socket import socket
+
+def accept(server_socket):
+    global clients
+    while True:
+        conn, address = server_socket.accept()
+        clients.append(conn)
+
+
+def communicate(server_socket):
+    while True:
+        for client in clients:
+            data = client.recv(1024).decode()
+            print(data)
+            for i in clients:
+                if client is not i:
+                    i.send(data.encode())
+
+
+def server():
+    server_socket = socket()
+    host = '127.0.0.1'
+    port = 5000
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    T1 = threading.Thread(target=accept, args=[server_socket])
+    T2 = threading.Thread(target=communicate, args=[server_socket])
+    T1.start()
+    T2.start()
+
+
+if __name__ == '__main__':
+    clients = []
+    server()
