4152ef4b3c7b9155f1003284acab112066676a08
Raph-Romeo <95073137+Raph-Romeo@users.noreply.github.com>
2022-11-18 10:40:34 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,26 +1,30 @@
 import threading
 from socket import socket
+import sys
+
 
 def accept(server_socket):
     global clients
     while True:
         conn, address = server_socket.accept()
         clients.append(conn)
+        threading.Thread(target=communicate, args=[conn]).start()
 
 
-def communicate(server_socket):
+def communicate(client):
     global clients
     while True:
-        for client in clients:
-            try:
-                data = client.recv(1024).decode()
-                print(data)
-                for i in clients:
-                    if client is not i:
-                        i.send(data.encode())
-            except:
-                client.close()
-                clients.remove(client)
+        try:
+            data = client.recv(1024).decode()
+            print(data)
+            for i in clients:
+                if client is not i:
+                    i.send(data.encode())
+        except:
+            client.close()
+            clients.remove(client)
+            print('server > Disconnected client')
+            sys.exit()
 
 
 def server():
@@ -28,11 +32,9 @@ def server():
     host = '127.0.0.1'
     port = 5000
     server_socket.bind((host, port))
-    server_socket.listen(1)
+    server_socket.listen(10)
     T1 = threading.Thread(target=accept, args=[server_socket])
-    T2 = threading.Thread(target=communicate, args=[server_socket])
     T1.start()
-    T2.start()
 
 
 if __name__ == '__main__':
